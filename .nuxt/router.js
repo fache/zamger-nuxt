8a81385f75fe537b8e1a773dae6d6617ab49b3e3
Faruk Slinic <faruk.slinic@gmail.com>
2020-06-28 22:47:13 +0200

    dodajem rezamger

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,21 +3,21 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _073a54ee = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
-const _99ded268 = () => interopDefault(import('../pages/ispiti/trash.vue' /* webpackChunkName: "pages/ispiti/trash" */))
-const _911b315e = () => interopDefault(import('../pages/ispiti/_id/index.vue' /* webpackChunkName: "pages/ispiti/_id/index" */))
-const _740e4d90 = () => interopDefault(import('../pages/izvjestaji/_id/index.vue' /* webpackChunkName: "pages/izvjestaji/_id/index" */))
-const _dc4c8cd8 = () => interopDefault(import('../pages/konacnaocjena/_id/index.vue' /* webpackChunkName: "pages/konacnaocjena/_id/index" */))
-const _ff55980e = () => interopDefault(import('../pages/kvizovi/_id/index.vue' /* webpackChunkName: "pages/kvizovi/_id/index" */))
-const _03fb557d = () => interopDefault(import('../pages/obavjestenja/_id/index.vue' /* webpackChunkName: "pages/obavjestenja/_id/index" */))
-const _68249921 = () => interopDefault(import('../pages/opcijepredmeta/_id/index.vue' /* webpackChunkName: "pages/opcijepredmeta/_id/index" */))
-const _42392ab1 = () => interopDefault(import('../pages/projekti/_id/index.vue' /* webpackChunkName: "pages/projekti/_id/index" */))
-const _59b66b72 = () => interopDefault(import('../pages/raspored/_id/index.vue' /* webpackChunkName: "pages/raspored/_id/index" */))
-const _73d257f6 = () => interopDefault(import('../pages/sistembodovanja/_id/index.vue' /* webpackChunkName: "pages/sistembodovanja/_id/index" */))
-const _aa9d02ea = () => interopDefault(import('../pages/uredigrupe/_id/index.vue' /* webpackChunkName: "pages/uredigrupe/_id/index" */))
-const _f6185482 = () => interopDefault(import('../pages/zadace/_id/index.vue' /* webpackChunkName: "pages/zadace/_id/index" */))
-const _f4c5f6ac = () => interopDefault(import('../pages/predmet/_idpredmeta/grupa/_idgrupe/index.vue' /* webpackChunkName: "pages/predmet/_idpredmeta/grupa/_idgrupe/index" */))
-const _3d92b1d7 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _4d01ce2b = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
+const _4372a222 = () => interopDefault(import('../pages/ispiti/trash.vue' /* webpackChunkName: "pages/ispiti/trash" */))
+const _a86c9418 = () => interopDefault(import('../pages/ispiti/_id/index.vue' /* webpackChunkName: "pages/ispiti/_id/index" */))
+const _0af21e5b = () => interopDefault(import('../pages/izvjestaji/_id/index.vue' /* webpackChunkName: "pages/izvjestaji/_id/index" */))
+const _1dae3d91 = () => interopDefault(import('../pages/konacnaocjena/_id/index.vue' /* webpackChunkName: "pages/konacnaocjena/_id/index" */))
+const _16e7b9b6 = () => interopDefault(import('../pages/kvizovi/_id/index.vue' /* webpackChunkName: "pages/kvizovi/_id/index" */))
+const _20904440 = () => interopDefault(import('../pages/obavjestenja/_id/index.vue' /* webpackChunkName: "pages/obavjestenja/_id/index" */))
+const _56e094c4 = () => interopDefault(import('../pages/opcijepredmeta/_id/index.vue' /* webpackChunkName: "pages/opcijepredmeta/_id/index" */))
+const _7df75c94 = () => interopDefault(import('../pages/projekti/_id/index.vue' /* webpackChunkName: "pages/projekti/_id/index" */))
+const _0ee2fc2a = () => interopDefault(import('../pages/raspored/_id/index.vue' /* webpackChunkName: "pages/raspored/_id/index" */))
+const _5c95d0b3 = () => interopDefault(import('../pages/sistembodovanja/_id/index.vue' /* webpackChunkName: "pages/sistembodovanja/_id/index" */))
+const _20aa78a4 = () => interopDefault(import('../pages/uredigrupe/_id/index.vue' /* webpackChunkName: "pages/uredigrupe/_id/index" */))
+const _794b2462 = () => interopDefault(import('../pages/zadace/_id/index.vue' /* webpackChunkName: "pages/zadace/_id/index" */))
+const _f1a94b66 = () => interopDefault(import('../pages/predmet/_idpredmeta/grupa/_idgrupe/index.vue' /* webpackChunkName: "pages/predmet/_idpredmeta/grupa/_idgrupe/index" */))
+const _f94ba9d8 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -37,63 +37,63 @@ export const routerOptions = {
 
   routes: [{
     path: "/login",
-    component: _073a54ee,
+    component: _4d01ce2b,
     name: "login"
   }, {
     path: "/ispiti/trash",
-    component: _99ded268,
+    component: _4372a222,
     name: "ispiti-trash"
   }, {
     path: "/ispiti/:id?",
-    component: _911b315e,
+    component: _a86c9418,
     name: "ispiti-id"
   }, {
     path: "/izvjestaji/:id?",
-    component: _740e4d90,
+    component: _0af21e5b,
     name: "izvjestaji-id"
   }, {
     path: "/konacnaocjena/:id?",
-    component: _dc4c8cd8,
+    component: _1dae3d91,
     name: "konacnaocjena-id"
   }, {
     path: "/kvizovi/:id?",
-    component: _ff55980e,
+    component: _16e7b9b6,
     name: "kvizovi-id"
   }, {
     path: "/obavjestenja/:id?",
-    component: _03fb557d,
+    component: _20904440,
     name: "obavjestenja-id"
   }, {
     path: "/opcijepredmeta/:id?",
-    component: _68249921,
+    component: _56e094c4,
     name: "opcijepredmeta-id"
   }, {
     path: "/projekti/:id?",
-    component: _42392ab1,
+    component: _7df75c94,
     name: "projekti-id"
   }, {
     path: "/raspored/:id?",
-    component: _59b66b72,
+    component: _0ee2fc2a,
     name: "raspored-id"
   }, {
     path: "/sistembodovanja/:id?",
-    component: _73d257f6,
+    component: _5c95d0b3,
     name: "sistembodovanja-id"
   }, {
     path: "/uredigrupe/:id?",
-    component: _aa9d02ea,
+    component: _20aa78a4,
     name: "uredigrupe-id"
   }, {
     path: "/zadace/:id?",
-    component: _f6185482,
+    component: _794b2462,
     name: "zadace-id"
   }, {
     path: "/predmet/:idpredmeta?/grupa/:idgrupe?",
-    component: _f4c5f6ac,
+    component: _f1a94b66,
     name: "predmet-idpredmeta-grupa-idgrupe"
   }, {
     path: "/",
-    component: _3d92b1d7,
+    component: _f94ba9d8,
     name: "index"
   }],
 
